cf68bdb6bd69baa2148a5880c102db93642fe3af
Hieu <hieuasroma@gmail.com>
2025-02-22 11:00:12 +0700

    "LOL"

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from src.image_gen import image_gen
 from src.text_to_video import text_to_video
 import os
 def remove_cache(folder_path):
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)  # Tạo thư mục nếu chưa tồn tại
     for file_name in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file_name)
         if os.path.isfile(file_path):  # Kiểm tra nếu là file
